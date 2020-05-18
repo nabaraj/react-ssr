a573b285b11be441a66557cc6b4c3576cc7fbd7f
Nabaraj Saha <nabara2k@gmail.com>
2020-05-18 09:52:16 +0530

    clear homepage

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -1,37 +1,13 @@
-/* eslint-disable no-script-url */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
+
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
-import PropTypes from 'prop-types'; // ES6
-import { fetchArticles } from '../actions';
-import ArticleDetailModal from '../components/ArticleDetailModal';
-
-const HomePage = props => {
-  const [modal, setModal] = useState(false);
-  const [currentArticle, setCurrentArticle] = useState({});
-
-  const readArticle = article => {
-    setCurrentArticle(article);
-    setModal(true);
-  };
-
-  const closeModal = () => {
-    setModal(false);
-  };
 
 
-
-  const { fetchArticles: loadArticles } = props;
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    loadArticles();
-  }, [loadArticles]);
+const HomePage = () => {
 
   return (
     <div>
-      {modal ? <ArticleDetailModal handler={closeModal} data={currentArticle} /> : null}
+
       <div className="row">
         <div className="section">
           <h3>Home</h3>
@@ -47,23 +23,4 @@ const HomePage = props => {
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    articles: state.articles
-  };
-};
-
-HomePage.propTypes = {
-  articles: PropTypes.arrayOf(PropTypes.any),
-  fetchArticles: PropTypes.func
-};
-
-HomePage.defaultProps = {
-  articles: [],
-  fetchArticles: null
-};
-
-export default connect(
-  mapStateToProps,
-  { fetchArticles }
-)(HomePage);
+export default HomePage
